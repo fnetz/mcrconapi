044a8f6e975798748b89c8397df8c3161f0af8a1
Felix Solcher <games.feliso@gmail.com>
2017-04-11 15:22:41 +0200

    Improved RConClient tests

--- src/test/java/org/fnet/mcrconapi/RConClientTest.java
+++ src/test/java/org/fnet/mcrconapi/RConClientTest.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
@@ -16,26 +16,28 @@ public class RConClientTest {
 	static final String PASSWORD = "rbfH3x";
 	static final String SHORT_COMMAND_RESPONSE = "shortresponse";
 	static final String SHORT_COMMAND_REQUEST = "shortrequest";
-	private RConSingleClientTestServer rConTestServer;
+	private static RConSingleClientTestServer rConTestServer;
+	private static int rconPort;
 	
 	@Rule
 	public Timeout globalTimeout = new Timeout(2, TimeUnit.SECONDS);
 
-	@Before
-	public void setUp() throws Exception {
+	@BeforeClass
+	public static void setUp() throws Exception {
 		rConTestServer = new RConSingleClientTestServer();
 		rConTestServer.start();
+		rconPort = rConTestServer.getPort();
 	}
 	
-	@After
-	public void tearDown() throws Exception {
+	@AfterClass
+	public static void tearDown() throws Exception {
 		rConTestServer.close();
 		rConTestServer = null;
 	}
 
 	@Test
 	public void testAuthenticate() throws IOException, AuthenticationException {
-		try (RConClient client = new RConClient("127.0.0.1")) {
+		try (RConClient client = new RConClient("127.0.0.1", rconPort)) {
 			client.authenticate(PASSWORD);
 			assertTrue(client.isAuthenticated());
 		}
@@ -43,29 +45,29 @@ public class RConClientTest {
 
 	@Test
 	public void testAuthenticateInConstructor() throws IOException, AuthenticationException {
-		try (RConClient client = new RConClient("127.0.0.1", PASSWORD)) {
+		try (RConClient client = new RConClient("127.0.0.1", rconPort, PASSWORD)) {
 			assertTrue(client.isAuthenticated());
 		}
 	}
 
 	@Test(expected = AuthenticationException.class)
 	public void testAuthenticateThrowsExceptionOnWrongPassword() throws IOException, AuthenticationException {
-		try (RConClient client = new RConClient("127.0.0.1")) {
+		try (RConClient client = new RConClient("127.0.0.1", rconPort)) {
 			client.authenticate("wrongPassword");
 		}
 	}
 
 	@Test
 	public void testSendCommand() throws IOException, AuthenticationException {
-		try (RConClient client = new RConClient("127.0.0.1")) {
+		try (RConClient client = new RConClient("127.0.0.1", rconPort)) {
 			client.authenticate(PASSWORD);
 			assertEquals(SHORT_COMMAND_RESPONSE, client.sendCommand(SHORT_COMMAND_REQUEST));
 		}
 	}
 
-	@Test(expected = InvalidPacketException.class, timeout = 0)
+	@Test(expected = InvalidPacketException.class)
 	public void testSendCommandThrowsExceptionOnUnknownCommand() throws IOException, AuthenticationException {
-		try (RConClient client = new RConClient("127.0.0.1")) {
+		try (RConClient client = new RConClient("127.0.0.1", rconPort)) {
 			client.authenticate(PASSWORD);
 			client.sendCommand("unknownCommand");
 		}
--- src/test/java/org/fnet/mcrconapi/RConSingleClientTestServer.java
+++ src/test/java/org/fnet/mcrconapi/RConSingleClientTestServer.java
@@ -4,54 +4,76 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
 
 final class RConSingleClientTestServer implements Runnable, AutoCloseable {
 
-	private Socket client;
-	private ServerSocket server;
-	private Thread serverThread;
+	private final ServerSocket server;
+	private final Thread serverThread;
+	private final List<Thread> clientThreads;
+	private final List<Socket> clientSockets;
+	private final int port;
 
-	public RConSingleClientTestServer() {
+	public RConSingleClientTestServer() throws IOException {
 		serverThread = new Thread(this);
+		server = new ServerSocket();
+		server.bind(null);
+		this.port = server.getLocalPort();
+		clientThreads = new ArrayList<>();
+		clientSockets = new ArrayList<>();
 	}
 
 	@Override
 	public void run() {
 		try {
-			server = new ServerSocket(25575);
-			client = server.accept();
-			while (server.isBound() && client.isConnected()) {
-				Packet packet = Packet.readFrom(client.getInputStream(), true);
-				switch (packet.getType()) {
-				case AUTH:
-					Packet response = new Packet(PacketType.AUTH_RESPONSE, "");
-					if (packet.getPayloadAsString().equals(RConClientTest.PASSWORD)) {
-						response.setRequestID(packet.getRequestID());
-					} else {
-						response.setRequestID(-1);
+			while (server.isBound()) {
+				Socket client = server.accept();
+				clientSockets.add(client);
+				Thread clientThread = new Thread(() -> {
+					try {
+						while (client.isConnected()) {
+							Packet packet = Packet.readFrom(client.getInputStream(), true);
+							switch (packet.getType()) {
+							case AUTH:
+								Packet response = new Packet(PacketType.AUTH_RESPONSE, "");
+								if (packet.getPayloadAsString().equals(RConClientTest.PASSWORD)) {
+									response.setRequestID(packet.getRequestID());
+								} else {
+									response.setRequestID(-1);
+								}
+								response.writeTo(client.getOutputStream());
+								break;
+							case COMMAND:
+								if (packet.getPayloadAsString().equals(RConClientTest.SHORT_COMMAND_REQUEST)) {
+									Packet crsp = new Packet(PacketType.COMMAND_RESPONSE,
+											RConClientTest.SHORT_COMMAND_RESPONSE);
+									crsp.writeTo(client.getOutputStream());
+								} else {
+									Packet crsp = new Packet(PacketType.COMMAND_RESPONSE, "");
+									crsp.writeTo(client.getOutputStream());
+								}
+								break;
+							default:
+								System.err.println("INVALID REQUEST TYPE " + packet.getType());
+								Packet errrsp = new Packet(PacketType.AUTH_RESPONSE, "");
+								errrsp.setRequestID(Packet.REQUEST_ID_AUTH_FAIL);
+								errrsp.writeTo(client.getOutputStream());
+								break;
+							}
+						}
+					} catch (EOFException e) {
+						// Ignore: this means that client closed connection
+					} catch (Exception e) {
+						e.printStackTrace();
 					}
-					response.writeTo(client.getOutputStream());
-					break;
-				case COMMAND:
-					if (packet.getPayloadAsString().equals(RConClientTest.SHORT_COMMAND_REQUEST)) {
-						Packet crsp = new Packet(PacketType.COMMAND_RESPONSE, RConClientTest.SHORT_COMMAND_RESPONSE);
-						crsp.writeTo(client.getOutputStream());
-					} else {
-						Packet crsp = new Packet(PacketType.COMMAND_RESPONSE, "");
-						crsp.writeTo(client.getOutputStream());
-					}
-					break;
-				default:
-					System.err.println("INVALID REQUEST TYPE " + packet.getType());
-					Packet errrsp = new Packet(PacketType.AUTH_RESPONSE, "");
-					errrsp.setRequestID(Packet.REQUEST_ID_AUTH_FAIL);
-					errrsp.writeTo(client.getOutputStream());
-					break;
-				}
+				});
+				clientThread.start();
 			}
-		} catch (EOFException e) {
-			// Ignore: this means that client closed connection
-		} catch (IOException e) {
+		} catch (SocketException e) {
+			// Ignore: this means that the server socket was closed
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -62,10 +84,18 @@ final class RConSingleClientTestServer implements Runnable, AutoCloseable {
 
 	@Override
 	public void close() throws Exception {
-		if (!client.isClosed())
-			client.close();
+		for (Socket s : clientSockets)
+			if (!s.isClosed())
+				s.close();
+		for (Thread t : clientThreads)
+			if (t.isAlive())
+				t.join();
 		if (!server.isClosed())
 			server.close();
 		serverThread.join();
+	}
+
+	public int getPort() {
+		return port;
 	}
 }
